30eeb2c9ae66ab15008ac91b6aa387dfe59bddc5
Michael Risser <mdrisser1@pm.me>
2025-07-22 13:36:08 -0700

    Restructure to use Javascript AJAX to fetch weather

--- www/index.php
+++ www/index.php
@@ -26,21 +26,8 @@ require_once("inc/config.php");
                 </div>
             </header>
         <!-- WEATHER -->
-            <div class="columns">
-                <div class="column col-3 wx">
-                    <div>Bullhead City WX CONDX</div>
-                    <div class="h2 temp"><?= round($bhc_wx->main->temp, 0); ?>&deg;F</div>
-                    <div class="wind-speed">Wind: <?= round($bhc_wx->wind->speed, 0); ?>MPH (<?= $bhc_wx->wind->deg; ?>)</div>
-                    <div class="wind-gust">Gust: <?= round($bhc_wx->wind->gust, 0); ?>MPH</div>
-                    <div class="humidity">Humidity: <?= round($bhc_wx->main->humidity, 0); ?>%</div>
-                </div>
-                 <div class="column col-3 wx">
-                    <div>Kingman WX CONDX</div>
-                    <div class="h2 temp"><?= round($kmn_wx->main->temp, 0); ?>&deg;F</div>
-                    <div class="wind-speed">Wind: <?= round($kmn_wx->wind->speed, 0); ?>MPH (<?= $kmn_wx->wind->deg; ?>)</div>
-                    <div class="wind-gust">Gust: <?= round($kmn_wx->wind->gust, 0); ?>MPH</div>
-                    <div class="humidity">Humidity: <?= round($kmn_wx->main->humidity, 0); ?>%</div>
-                </div>
+            <div id="weather" class="columns">
+                
             </div>
         <!-- RSS FEEDS -->
             <?php
@@ -115,6 +102,27 @@ require_once("inc/config.php");
     <!-- JAVASCRIPT -->
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
         <script>
+        const base_url = 'http://api.weatherapi.com/v1/current.json?key=';
+        const apikey = '';
+        const wx_url = base_url + apikey + '&q=';
+        const bhc = '86442';
+        const kmn = '86409';
+        const prs = '86301';
+        const flg = '86001';
+
+        function get_wx(el, $url, $loc) {
+            $.getJSON($url + $loc, function(data) {
+                let html = "<div class='column col-3 wx'>";
+                html += "<div><a href='weather.html?q="+$loc+"'>" + data['location']['name'] + " WX CONDX</a></div>";
+                html += "<div class='h2 temp'>" + Math.round(data['current']['temp_f']) + "&deg;F</div>";
+                html += "<div class='wind-speed'>Wind: " + Math.round(data['current']['wind_mph']) + "mph (" + data['current']['wind_dir'] + ")</div>";
+                html += "<div class='wind-gust'>Gust: " + Math.round(data['current']['gust_mph']) +"mph</div>";
+                html += "<div class='humidity'>Humidity: " + Math.round(data['current']['humidity']) + "%</div>";
+
+                $(el).append(html);
+            });
+        }
+
         $(document).ready(function() {
             $('.card-body').each(function() {
                 $(this).slideToggle("fast");
@@ -129,6 +137,11 @@ require_once("inc/config.php");
                     $(this).removeClass('icon-arrow-up').addClass('icon-arrow-down');
                 }
             });
+
+            get_wx('#weather', wx_url, bhc);
+            get_wx('#weather', wx_url, kmn);
+            get_wx('#weather', wx_url, prs);
+            get_wx('#weather', wx_url, flg);
         });
         </script>
     </body>
